63d168033489ffb2cb57df7acfa127e912fbfe55
Your Name <you@example.com>
2020-11-09 00:55:01 +0400

    initial-commit

---
+++ src/components/InputList/components/Input/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import './styles.css';
+
+const Input = ({
+  value: initialValue,
+  id,
+  addItem,
+  isLastItem,
+  removeItem,
+  changeItem,
+  inputRef,
+  setIndex,
+  index,
+}) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    if (initialValue === '' && value.length > 0 && isLastItem) {
+      addItem();
+    }
+  }, [value, addItem, initialValue, isLastItem]);
+
+  const handleOnChange = (e) => {
+    if (e.target.value === '') {
+      setIndex(index);
+      removeItem(id);
+    }
+    setValue(e.target.value);
+  };
+
+  const changeItemValue = () => {
+    if (initialValue !== value) {
+      changeItem({ id, value });
+    }
+  };
+
+  return (
+    <input
+      ref={inputRef}
+      value={value}
+      onChange={handleOnChange}
+      onBlur={changeItemValue}
+      onKeyPress={({ key }) => {
+        if (key === 'Enter' && value.length) {
+          changeItemValue();
+          setIndex(index + 1);
+        }
+      }}
+      onFocus={() => {
+        setIndex(index);
+      }}
+    />
+  );
+};
+
+export default Input;
